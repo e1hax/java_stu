b5485edd21cb7d38c08b763d5aa1649b2a5ff23d
e1hax <1641682890@qq.com>
2022-04-30 18:30:18 +0800

    22.4.30

---
+++ chapter13/src/com/javastu/stringbuffer/StringBuffer02.java
@@ -0,0 +1,9 @@
+package com.javastu.stringbuffer;
+
+public class StringBuffer02 {
+    public static void main(String[] args) {
+//        1.创建一个大小为 16的 char[]，用于存放字符内容
+
+        StringBuffer stringBuffer = new StringBuffer();
+    }
+}
---
+++ chapter13/src/com/javastu/stringbuilder_/StringBuilderVSStringBufferVSString.java
@@ -0,0 +1,37 @@
+package com.javastu.stringbuilder_;
+
+public class StringBuilderVSStringBufferVSString {
+    public static void main(String[] args) {
+        String text = "";
+        long stratTime =0L;
+        long endTime =0L;
+
+        //StringBuffer的执行时间
+        StringBuffer buffer = new StringBuffer("");
+        stratTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            buffer.append(String.valueOf(i));
+        }
+        endTime =System.currentTimeMillis();
+        System.out.println("StringBuffer的执行时间"+(endTime-stratTime));
+
+
+//        StringBuilder的执行时间
+        StringBuilder builder = new StringBuilder("");
+        stratTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            builder.append(String.valueOf(i));
+        }
+        endTime =System.currentTimeMillis();
+        System.out.println("StringBuilder的执行时间"+(endTime-stratTime));
+
+
+//        String的执行时间
+        stratTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            text = text+i;
+        }
+        endTime =System.currentTimeMillis();
+        System.out.println("String的执行时间"+(endTime-stratTime));
+    }
+}
